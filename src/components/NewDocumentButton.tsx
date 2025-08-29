122046c61a5cce33e4f50346f36b27df2e6e064a
Rahul <rahulp00rtx@gmail.com>
2025-08-29 22:31:20 +0530

    4 . Add user type definitions and implement document creation functionality
    - Created User type in types.ts for user data structure.
    - Added CustomJwtSessionClaims interface in globals.d.ts for JWT session claims.
    - Implemented createNewDocument function in actions.ts to handle document creation and user association.
    - Updated NewDocumentButton component to trigger document creation and navigate to the new document.

--- src/components/NewDocumentButton.tsx
+++ src/components/NewDocumentButton.tsx
@@ -1,6 +1,22 @@
+"use client";
+import { useTransition } from "react";
 import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
+import { createNewDocument } from "@/action/actions";
 
 function NewDocumentButton() {
-  return <Button>New Document</Button>;
+  const [isPending, startTranstion] = useTransition();
+  const router = useRouter();
+  const handleCreateNewDocument = () => {
+    startTranstion(async () => {
+      const { docId } = await createNewDocument();
+      router.push(`/doc/${docId}`);
+    });
+  };
+  return (
+    <Button onClick={handleCreateNewDocument} disabled={isPending}>
+      {isPending ? "Creating..." : "New Doucment"}
+    </Button>
+  );
 }
 export default NewDocumentButton;
