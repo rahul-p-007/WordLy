02615ac7a5dbed0d2a5426f517ff9be9126a1262
rahul-p-007 <rahulp00rtx@gmail.com>
2025-08-31 14:22:23 +0530

    6. Created user change the file name

--- src/app/doc/[id]/page.tsx
+++ src/app/doc/[id]/page.tsx
@@ -1,14 +1,11 @@
 "use client";
 
+import { use } from "react";
 import Document from "@/components/Document";
 
-function DocumentPage({
-  params: { id },
-}: {
-  params: {
-    id: string;
-  };
-}) {
+function DocumentPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = use(params);
+
   return (
     <div className="flex flex-col flex-1 min-h-screen">
       <Document id={id} />
--- src/components/Document.tsx
+++ src/components/Document.tsx
@@ -1,10 +1,42 @@
 "use client";
+
+import { FormEvent, useEffect, useState, useTransition } from "react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "@/firebase";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+
 function Document({ id }: { id: string }) {
+  const [data, loading, error] = useDocumentData(doc(db, "documents", id));
+  const [input, setInput] = useState("");
+  const [isUpdating, startTransition] = useTransition();
+
+  useEffect(() => {
+    if (data) {
+      setInput(data.title);
+    }
+  }, [data]);
+
+  const updateTitle = (e: FormEvent) => {
+    e.preventDefault();
+    if (input.trim()) {
+      startTransition(async () => {
+        await updateDoc(doc(db, "documents", id), {
+          title: input,
+        });
+      });
+    }
+  };
   return (
     <div>
-      <div>
-        <form action="">
+      <div className="flex max-w-6xl mx-auto justify-between pb-5">
+        <form className="flex  space-x-2 flex-1 " onSubmit={updateTitle}>
           {/* upate title */}
+          <Input value={input} onChange={(e) => setInput(e.target.value)} />
+          <Button disabled={isUpdating} type="submit">
+            {isUpdating ? "Updating..." : "Update"}
+          </Button>
 
           {/* IF */}
           {/* isOwner && Inviteuser,DeleteDocument */}
